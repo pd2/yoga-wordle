3cec6f8fb0740c57816e154cd0d3067baf5d56e3
Glitch (yoga-wordle) <none>
2025-05-27 09:58:23 +0000

    ☕🆕 Updated with Glitch

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticDev = "yoga_wordle"
+const service_worker_version = "v1.01"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
